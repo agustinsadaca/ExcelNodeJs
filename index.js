c5d22422e4febaefb3faece5060021e5d7723faf
agustinsadaca <agustinsadaca@gmail.com>
2022-01-12 02:26:15 -0300

    1

---
+++ index.js
@@ -0,0 +1,22 @@
+// Require library
+var excel = require('excel4node');
+
+// Create a new instance of a Workbook class
+var workbook = new excel.Workbook();
+
+// Add Worksheets to the workbook
+var worksheet = workbook.addWorksheet('Sheet 1');
+var worksheet2 = workbook.addWorksheet('Sheet 2');
+
+// Create a reusable style
+var style = workbook.createStyle({
+  font: {
+    color: '#FF0800',
+    size: 12
+  },
+  numberFormat: '$#,##0.00; ($#,##0.00); -'
+});
+// Set value of cell A1 to 100 as a number type styled with paramaters of style
+worksheet.cell(1,1).number(100).style(style);
+
+workbook.write('Excel.xlsx');
